33249d98ce9ae603b563bf9721818f75de3ce49b
Midhun <midhun2031@gmail.com>
2025-01-23 23:49:56 +0530

    Policy

--- src/components/FooterDetails/privacyPolicy.tsx
+++ src/components/FooterDetails/privacyPolicy.tsx
@@ -34,9 +34,10 @@ const PrivacyPolicy = () => {
       </section>
 
       <section className="mb-6">
-        <h2 className="md:text-2xl text-lg font-semibold text-gray-800 mb-4">2. Information We Collect</h2>
-        <h3 className="md:text-md text-base font-medium text-gray-700 mb-2">Personal Data</h3>
-        <p className="text-gray-700 mb-4">We may collect the following personal information:</p>
+        <h2 className="md:text-2xl text-lg font-semibold text-gray-800 mb-4">Collecting and using your personal data</h2>
+        <h3 className="md:text-md text-base font-medium text-gray-700 mb-2">Types of data collected</h3>
+        <p className="text-gray-700 mb-4">Personal Data</p>
+        <p className="text-gray-700 mb-4">While using Our Service, We may ask You to provide Us with certain personally identifiable information that can be used to contact or identify You. Personally identifiable information may include, but is not limited to:</p>
         <ul className="list-inside md:text-lg text-sm list-disc text-gray-700 mb-4">
           <li>Email address</li>
           <li>Name</li>
@@ -65,53 +66,154 @@ const PrivacyPolicy = () => {
           <li>For analyzing and improving our service and user experience</li>
         </ul>
       </section>
-
+     
       <section className="mb-6">
-        <h2 className="md:text-2xl text-lg font-semibold text-gray-800 mb-4">4. Sharing Your Information</h2>
-        <p className="text-gray-700 mb-4">We may share your personal information in the following situations:</p>
+        <h2 className="md:text-2xl text-lg font-semibold text-gray-800 mb-4">Information from Third-Party Social Media Services</h2>
+        <p className="text-gray-700 mb-4">The Company allows You to create an account and log in to use the Service through the following Third-party Social Media Services:</p>
         <ul className="list-inside md:text-lg text-sm list-disc text-gray-700">
-          <li>With Service Providers to help us monitor and analyze usage</li>
-          <li>For business transfers during mergers or asset sales</li>
-          <li>With our affiliates who are required to honor this policy</li>
-          <li>With business partners offering services or promotions</li>
-          <li>With your consent, we may disclose your data for other purposes</li>
+          <li>Google</li>
+          <li>Instagram</li>
+          <li>Facebook</li>
+          <li>Twitter</li>
+          <li>Linkedin</li>
+          <li>Youtube</li>
         </ul>
+        <p className="text-gray-700 mb-4"> <p className="text-gray-700 mb-4">The Company allows You to create an account and log in to use the Service through the following Third-party Social Media Services:</p></p>
       </section>
-
       <section className="mb-6">
-        <h2 className="md:text-2xl text-lg font-semibold text-gray-800 mb-4">5. Retention of Your Personal Data</h2>
-        <p className="text-gray-700 mb-4">
-          We will retain your personal data only for as long as necessary for the purposes outlined in this policy. This data
-          will be kept to comply with legal obligations, resolve disputes, and enforce our policies.
-        </p>
-      </section>
-
-      <section className="mb-6">
-        <h2 className="md:text-2xl text-lg font-semibold text-gray-800 mb-4">6. Security of Your Personal Data</h2>
-        <p className="text-gray-700 mb-4">
-          We strive to use commercially acceptable means to protect your personal data, but we cannot guarantee absolute security.
-          Please take precautions when using the service.
-        </p>
-      </section>
-
-      <section className="mb-6">
-        <h2 className="md:text-2xl text-lg font-semibold text-gray-800 mb-4">7. Changes to this Privacy Policy</h2>
-        <p className="text-gray-700 mb-4">
-          We may update this Privacy Policy from time to time. Any changes will be posted on this page with an updated date.
-        </p>
-      </section>
-
-      <section className="mb-6">
-        <h2 className="md:text-2xl text-lg font-semibold text-gray-800 mb-4">8. Contact Us</h2>
-        <p className="text-gray-700 mb-4">
-          If you have any questions about this Privacy Policy, feel free to contact us:
-        </p>
+        <h2 className="md:text-2xl text-lg font-semibold text-gray-800 mb-4">Tracking Technologies and Cookies</h2>
+        <p className="text-gray-700 mb-4">We use Cookies and similar tracking technologies to track the activity on Our Service and store certain information. Tracking technologies used are beacons, tags, and scripts to collect and track information and to improve and analyze Our Service. The technologies We use may include:</p>
         <ul className="list-inside md:text-lg text-sm list-disc text-gray-700">
-          <li>Email: <a href="mailto:support@stilesagio.com" className="text-blue-500">support@stilesagio.com</a></li>
-          <li>Phone: +91 9677966333</li>
-          <li>Address: TVT TEXTILES, 65, 6th Street, Padmini Gardens, Kangeyam Road, Tirupur - 641604, India</li>
+          <li><span className="font-semibold">Cookies or Browser Cookies</span>. A cookie is a small file placed on Your Device. You can instruct Your browser to refuse all Cookies or to indicate when a Cookie is being sent. However, if You do not accept Cookies, You may not be able to use some parts of our Service. Unless you have adjusted Your browser setting so that it will refuse Cookies, our Service may use Cookies.</li>
+          <li><span className="font-semibold">Web Beacons</span>Certain sections of our Service and our emails may contain small electronic files known as web beacons (also referred to as clear gifs, pixel tags, and single-pixel gifs) that permit the Company, for example, to count users who have visited those pages or opened an email and for other related website statistics (for example, recording the popularity of a certain section and verifying system and server integrity).</li>
+         
         </ul>
+        <p className="text-gray-700 mb-4"> <p className="text-gray-700 mb-4">Necessary / Essential Cookies
+Type: Session Cookies <br />
+Administered by: Us <br />
+Purpose: These Cookies are essential to provide You with services available through the Website and to enable You to use some of its features. They help to authenticate users and prevent fraudulent use of user accounts. Without these Cookies, the services that You have asked for cannot be provided, and We only use these Cookies to provide You with those services.
+Cookies Policy / Notice Acceptance Cookies <br />
+Type: Persistent Cookies <br />
+Administered by: Us <br />
+Purpose: These Cookies identify if users have accepted the use of cookies on the Website. <br />
+Functionality Cookies <br />
+Type: Persistent Cookies <br />
+Administered by: Us <br />
+Purpose: These Cookies allow us to remember choices You make when You use the Website, such as remembering your login details or language preference. The purpose of these Cookies is to provide You with a more personal experience and to avoid You having to re-enter your preferences every time You use the Website.
+For more information about the cookies we use and your choices regarding cookies, please visit our Cookies Policy or the Cookies section of our Privacy Policy.
+</p></p>
       </section>
+      <section className="mb-6">
+        <h2 className="md:text-2xl text-lg font-semibold text-gray-800 mb-4">Use of Your Personal Data</h2>
+        <p className="text-gray-700 mb-4">The Company may use Personal Data for the following purposes:
+To provide and maintain our Service, including to monitor the usage of our Service.
+To manage Your Account: to manage Your registration as a user of the Service. The Personal Data You provide can give You access to different functionalities of the Service that are available to You as a registered user.
+For the performance of a contract: the development, compliance and undertaking of the purchase contract for the products, items or services You have purchased or of any other contract with Us through the Service.
+To contact You: To contact You by email, telephone calls, SMS, or other equivalent forms of electronic communication, such as a mobile application's push notifications regarding updates or informative communications related to the functionalities, products or contracted services, including the security updates, when necessary or reasonable for their implementation.
+To provide You with news, special offers and general information about other goods, services and events which we offer that are similar to those that you have already purchased or enquired about unless You have opted not to receive such information.
+To manage Your requests: To attend and manage Your requests to Us.
+For business transfers: We may use Your information to evaluate or conduct a merger, divestiture, restructuring, reorganization, dissolution, or other sale or transfer of some or all of Our assets, whether as a going concern or as part of bankruptcy, liquidation, or similar proceeding, in which Personal Data held by Us about our Service users is among the assets transferred.
+For other purposes: We may use Your information for other purposes, such as data analysis, identifying usage trends, determining the effectiveness of our promotional campaigns and to evaluate and improve our Service, products, services, marketing and your experience.
+We may share Your personal information in the following situations: <br />
+•	With Service Providers: We may share Your personal information with Service Providers to monitor and analyze the use of our Service, to contact You.  <br />
+•	For business transfers: We may share or transfer Your personal information in connection with, or during negotiations of, any merger, sale of Company assets, financing, or acquisition of all or a portion of Our business to another company.  <br />
+•	With Affiliates: We may share Your information with Our affiliates, in which case we will require those affiliates to honor this Privacy Policy. Affiliates include Our parent company and any other subsidiaries, joint venture partners or other companies that We control or that are under common control with Us.  <br />
+•	With business partners: We may share Your information with Our business partners to offer You certain products, services or promotions.
+•	With other users: when You share personal information or otherwise interact in the public areas with other users, such information may be viewed by all users and may be publicly distributed outside. If You interact with other users or register through a Third-Party Social Media Service, Your contacts on the Third-Party Social Media Service may see Your name, profile, pictures and description of Your activity. Similarly, other users will be able to view descriptions of Your activity, communicate with You and view Your profile.  <br />
+•	With Your consent: We may disclose Your personal information for any other purpose with Your consent.  <br />
+</p>  
+        </section>
+                <section className="mb-6">
+                <h2 className="md:text-2xl text-lg font-semibold text-gray-800 mb-4">Retention of Your Personal Data</h2>
+                <p className="text-gray-700 mb-4">The Company will retain Your Personal Data only for as long as is necessary for the purposes set out in this Privacy Policy. We will retain and use Your Personal Data to the extent necessary to comply with our legal obligations (for example, if we are required to retain your data to comply with applicable laws), resolve disputes, and enforce our legal agreements and policies.
+        The Company will also retain Usage Data for internal analysis purposes. Usage Data is generally retained for a shorter period of time, except when this data is used to strengthen the security or to improve the functionality of Our Service, or We are legally obligated to retain this data for longer time periods.
+
+        </p>
+         
+        </section>  
+        <section className="mb-6">
+                <h2 className="md:text-2xl text-lg font-semibold text-gray-800 mb-4">Transfer of Your Personal Data</h2>
+                <p className="text-gray-700 mb-4">
+                Your information, including Personal Data, is processed at the Company's operating offices and in any other places where the parties involved in the processing are located. It means that this information may be transferred to — and maintained on — computers located outside of Your state, province, country or other governmental jurisdiction where the data protection laws may differ than those from Your jurisdiction.
+Your consent to this Privacy Policy followed by Your submission of such information represents Your agreement to that transfer.
+The Company will take all steps reasonably necessary to ensure that Your data is treated securely and in accordance with this Privacy Policy and no transfer of Your Personal Data will take place to an organization or a country unless there are adequate controls in place including the security of Your data and other personal information.
+
+                </p>
+         
+        </section>  
+        <section className="mb-6">
+                <h2 className="md:text-2xl text-lg font-semibold text-gray-800 mb-4">Delete Your Personal Data</h2>
+                <p className="text-gray-700 mb-4">
+                You have the right to delete or request that We assist in deleting the Personal Data that We have collected about You.
+Our Service may give You the ability to delete certain information about You from within the Service.
+You may update, amend, or delete Your information at any time by signing in to Your Account, if you have one, and visiting the account settings section that allows you to manage Your personal information. You may also contact Us to request access to, correct, or delete any personal information that You have provided to Us.
+Please note, however, that We may need to retain certain information when we have a legal obligation or lawful basis to do so.
+
+                </p>
+         
+        </section>  
+        <section className="mb-6">
+                <h2 className="md:text-2xl text-lg font-semibold text-gray-800 mb-4">Disclose of Your Personal Data</h2>
+                <p className="text-gray-700 mb-4">
+                Business Transactions
+If the Company is involved in a merger, acquisition or asset sale, Your Personal Data may be transferred. We will provide notice before Your Personal Data is transferred and becomes subject to a different Privacy Policy.
+Law enforcement
+Under certain circumstances, the Company may be required to disclose Your Personal Data if required to do so by law or in response to valid requests by public authorities (e.g. a court or a government agency).
+Other legal requirements
+The Company may disclose Your Personal Data in the good faith belief that such action is necessary to: <br />
+•	Comply with a legal obligation  <br />
+•	Protect and defend the rights or property of the Company  <br />
+•	Prevent or investigate possible wrongdoing in connection with the Service  <br />
+•	Protect the personal safety of Users of the Service or the public  <br />
+•	Protect against legal liability  <br />
+                </p>
+        </section> 
+        <section className="mb-6">
+                <h2 className="md:text-2xl text-lg font-semibold text-gray-800 mb-4">Security of Your Personal Data</h2>
+                <p className="text-gray-700 mb-4">
+                The security of Your Personal Data is important to Us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While We strive to use commercially acceptable means to protect Your Personal Data, We cannot guarantee its absolute security.
+                </p>
+         
+        </section> 
+        <section className="mb-6">
+                <h2 className="md:text-2xl text-lg font-semibold text-gray-800 mb-4">Children Privacy</h2>
+                <p className="text-gray-700 mb-4">
+                Our Service does not address anyone under the age of 13. We do not knowingly collect personally identifiable information from anyone under the age of 13. If You are a parent or guardian and You are aware that Your child has provided Us with Personal Data, please contact Us. If We become aware that We have collected Personal Data from anyone under the age of 13 without verification of parental consent, We take steps to remove that information from Our servers.
+If We need to rely on consent as a legal basis for processing Your information and Your country requires consent from a parent, We may require Your parent's consent before We collect and use that information.
+                </p>
+        </section>   
+        <section className="mb-6">
+                <h2 className="md:text-2xl text-lg font-semibold text-gray-800 mb-4">Link to other websites</h2>
+                <p className="text-gray-700 mb-4">
+                Our Service may contain links to other websites that are not operated by Us. If You click on a third party link, You will be directed to that third party's site. We strongly advise You to review the Privacy Policy of every site You visit.
+We have no control over and assume no responsibility for the content, privacy policies or practices of any third party sites or services.
+
+                </p>
+         
+        </section>   
+        <section className="mb-6">
+                <h2 className="md:text-2xl text-lg font-semibold text-gray-800 mb-4">Changes to the other policies</h2>
+                <p className="text-gray-700 mb-4">
+                We may update Our Privacy Policy from time to time. We will notify You of any changes by posting the new Privacy Policy on this page.
+We will let You know via email and/or a prominent notice on Our Service, prior to the change becoming effective and update the "Last updated" date at the top of this Privacy Policy.
+You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page.
+
+                </p>
+         
+        </section>   
+        <section className="mb-6">
+                <h2 className="md:text-2xl text-lg font-semibold text-gray-800 mb-4">Contact Us</h2>
+                <p className="text-gray-700 mb-4">
+                If you have any questions about this Privacy Policy, You can contact us:  <br />
+By email: (support@stilesagio.com) /( tvt4textiles@gmail.com)  <br />
+By visiting this page on our website: www.stilesagio.com  <br />
+By phone number: +919677966333  <br />
+By mail: TVT TEXTILES, No 65,6th Street, Padmini Gardens , Kangeyam Road,Tirupur-641604 . INDIA  <br />
+GSTIN NO : 33AVSPT6748B1Z5  <br />
+
+                </p>
+         
+        </section>   
     </div>
   );
 };
