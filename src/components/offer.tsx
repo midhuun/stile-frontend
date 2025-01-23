3f4568453835916ec76fd33cc71d6dbab24f3322
Midhun <midhun2031@gmail.com>
2025-01-23 11:40:34 +0530

    Update Policy

--- src/components/offer.tsx
+++ src/components/offer.tsx
@@ -4,7 +4,6 @@ const Offer = () => {
   const arr = Array(10).fill(brand)
   return (
     <div className="md:text-xs md:h-[50px] h-[45px] font-semibold fixed top-0 w-full  text-white z-[900] bg-black  text-[10px] flex justify-center items-center text-center">
-    <p>Buy 3 stylish collar T-shirts for just ₹999—limited offer!</p>
     <div className="flex justify-center items-center animate-scroll whitespace-nowrap">
        {arr.map((__,index:any)=>
        <div key={index} className="flex gap-3">
