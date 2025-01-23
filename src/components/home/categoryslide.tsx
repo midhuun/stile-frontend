273a43c9cc2d976ac33bec65e522ca718a96fc2b
Midhun <midhun2031@gmail.com>
2025-01-23 11:37:09 +0530

    Update Policy

--- src/components/home/categoryslide.tsx
+++ src/components/home/categoryslide.tsx
@@ -22,7 +22,7 @@ const Categories = () => {
     <div className='flex px-2 md:px-3 gap-2 md:gap-3 min-h-[320px] py-3 md:py-7 overflow-x-auto '>
     {categories.length>0 ? categories.map((category:any)=>
     <div className=' flex flex-col min-w-[140px] md:min-w-[350px] sm:w-[32%] justify-center gap-1 md:gap-4' key={category._id}>
-        <Link to={`/subcategory/${category.slug}`}><img loading="lazy" className='rounded-md h-[230px] xs:h-[300px]   border w-full md:h-[450px]  object-top object-cover ' src={`${category.image}?w=400&h=500&q=75`} alt="" /></Link>
+        <Link to={`/subcategory/${category.slug}`}><img loading="lazy" className='rounded-md h-[230px] xs:h-[300px] w-full md:h-[450px]  object-top object-cover ' src={`${category.image}?w=400&h=500&q=75`} alt="" /></Link>
         <p className='text-[12px] overflow-hidden  break-words h-10 md:text-[18px] '>{category.name}</p>
     </div>
     ):null}
