5a4842857920698b9655f84833bef7f717c95a1d
Midhun <midhun2031@gmail.com>
2025-01-23 11:38:23 +0530

    Update Policy

--- src/components/offer.tsx
+++ src/components/offer.tsx
@@ -1,15 +1,14 @@
 
 const Offer = () => {
-  const brand = "Stile Sagio"
+  const brand = ""
   const arr = Array(10).fill(brand)
   return (
     <div className="md:text-xs md:h-[50px] h-[45px] font-semibold fixed top-0 w-full  text-white z-[900] bg-black  text-[10px] flex justify-center items-center text-center">
     <p>Buy 3 stylish collar T-shirts for just ₹999—limited offer!</p>
     <div className="flex justify-center items-center animate-scroll whitespace-nowrap">
-       {arr.map((val:any,index:any)=>
-       <div className="flex gap-3">
+       {arr.map((__,index:any)=>
+       <div key={index} className="flex gap-3">
         <img className="h-7 object-contain" src="/logoanimation.png" alt="" />
-        <h1 key={index} className="text-white font-light uppercase md:text-sm text-[12px] ">{val}</h1>
         </div>
       )}
        
