996196a56f4cea9ba91198e1ad86e7213bb25ea9
Midhun <midhun2031@gmail.com>
2025-01-23 12:41:05 +0530

    Update Policy

--- src/components/offer.tsx
+++ src/components/offer.tsx
@@ -1,20 +1,17 @@
 
 const Offer = () => {
-  const brand = ""
-  const arr = Array(10).fill(brand)
-  return (
-    <div className="md:text-xs md:h-[50px] h-[45px] font-semibold fixed top-0 w-full  text-white z-[900] bg-black  text-[10px] flex justify-center items-center text-center">
-    <div className="flex justify-center items-center animate-scroll whitespace-nowrap">
-       {arr.map((__,index:any)=>
-       <div key={index} className="flex gap-3">
-        <img className="h-7 object-contain" src="/logoanimation.png" alt="" />
-        </div>
-      )}
-       
-      </div>
-    <hr />
-    </div>
-  )
-}
+  const arr = Array(10).fill("Stile Sagio");
 
-export default Offer
+  return (
+    <div className="md:text-xs md:h-[50px] h-[45px] font-semibold fixed top-0 w-full text-white z-[900] bg-black text-[10px] flex justify-center items-center text-center">
+      <div className="flex justify-start items-center animate-scroll whitespace-nowrap">
+        {arr.map((text, index) => (
+          <div key={index} className="mr-8">{text}</div>
+        ))}
+      </div>
+      <hr />
+    </div>
+  );
+};
+
+export default Offer;
