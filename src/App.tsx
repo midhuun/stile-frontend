1f0df43458f1f4c43b5fcb7491d9f0ea16bdad79
Midhun <midhun2031@gmail.com>
2025-02-16 10:16:54 +0530

    Change Design and Payment logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import './App.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Header from './components/header/header';
-import Footer from './components/footer/footer';
 import HeaderProvider from './context/appContext';
 import OtpLoginPopup from './components/login/login';
 import Offer from './components/offer';
 import { Suspense, lazy, useEffect } from 'react';
 import Loading from './components/loading/loading';
-import Home from './components/home/home';
-import PrivacyPolicy from './components/FooterDetails/privacyPolicy';
 import ScrollToTop from './components/home/scrollTop';
 import PaymentStatus from './components/cart/paymentStatus';
 import { useDispatch } from 'react-redux';
 import { fetchProducts } from './store/reducers/productReducer';
-import HomeSub from './components/home/homeSub';
-import OrderDetails from './account/order';
-import PaymentStatusPage from './components/cart/orderStatus';
+const PaymentStatusPage = lazy(() => import('./components/cart/orderStatus'));
+const PrivacyPolicy = lazy(() => import('./components/FooterDetails/privacyPolicy'));
+const OrderDetails = lazy(() => import('./account/order'));
+const HomeSub = lazy(() => import('./components/home/homeSub'));
+const Home = lazy(() => import('./components/home/home'));
+const Footer = lazy(()=>import('./components/footer/footer'))
 const About = lazy(() => import('./components/about/about'));
 const CategoryPage = lazy(() => import('./components/categoryPage/categoryPage'));
 const ProductPage = lazy(() => import('./components/product/ProductPage'));
