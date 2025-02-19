6c0b51e6206400d437ec9f820f393c7e58cfad1a
Midhun <midhun2031@gmail.com>
2025-02-19 15:14:57 +0530

    Optimized perfomance

--- src/components/home/homeSub.tsx
+++ src/components/home/homeSub.tsx
@@ -1,60 +1,69 @@
-import { SubCategory } from "../../types/CategoryType";
+import { useEffect, useState } from "react";
+import { SubCategory, Product } from "../../types/CategoryType";
 import ProductCard from "../product/productCard";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { RootState } from "../../store/store";
 const HomeSub = () => {
-  const items:any = useSelector((state:RootState)=>state.Products)
+  const [subcategories, setSubcategories] = useState<SubCategory[]>([]);
+  const [loading, setLoading] = useState(true);
   // Fetch categories from the backend
-  // async function fetchCategories() {
-  //   setLoading(true);
-  //   try {
-  //     const result = await fetch("https://stile-backend.vercel.app/products");
-  //     const data = await result.json();
-  //     setSubcategories(data.subCategories);
-  //   } catch (error) {
-  //     console.error("Error fetching subcategories:", error);
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // }
+  async function fetchCategories() {
+    setLoading(true);
+    try {
+      const result = await fetch("https://stile-backend.vercel.app/products");
+      const data = await result.json();
+      const processedSubcategories = data?.subCategories?.map((subcategory: SubCategory) => ({
+        ...subcategory,
+        products: removeDuplicateProducts(subcategory.products),
+      }));
+
+      setSubcategories(processedSubcategories);
+    } catch (error) {
+      console.error("Error fetching subcategories:", error);
+    } finally {
+      setLoading(false);
+    }
+  }
 
   // Remove duplicate products by name for a subcategory
-  // const removeDuplicateProducts = (products: Product[]) => {
-  //   const seenNames = new Set();
-  //   return products.filter((product) => {
-  //     if (!seenNames.has(product.name)) {
-  //       seenNames.add(product.name);
-  //       return true;
-  //     }
-  //     return false;
-  //   });
-  // };
+  const removeDuplicateProducts = (products: Product[]) => {
+    const seenNames = new Set();
+    return products.filter((product) => {
+      if (!seenNames.has(product.name)) {
+        seenNames.add(product.name);
+        return true;
+      }
+      return false;
+    });
+  };
 
-  // if (loading) {
-  //   return (
-  //     <div className="loading-container h-full w-full ">
-  //       {["1","2","3","4","5"].map((__,index:number)=><div key={index} className="flex min-h-[350px] md:justify-start hide-scroll overflow-x-scroll gap-2 py-4  md:gap-4 will-change-transform">
-  //       {["1","2","3","4","5"].map((__,index)=>
-  //       <div key={index}
-  //       className={`h-[300px] gap-4 flex flex-col min-w-[140px] md:min-w-[250px]  md:h-[520px] md:w-[300px] animate-pulse bg-gray-200 rounded-lg`}
-  //     >
-  //       {/* Image Skeleton */}
-  //       <div className="w-full h-[250px] md:h-[450px] bg-gray-300 rounded-md"></div>
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="loading-container h-full w-full ">
+        {["1","2","3","4","5"].map((__,index:number)=><div key={index} className="flex min-h-[350px] md:justify-start hide-scroll overflow-x-scroll gap-2 py-4  md:gap-4 will-change-transform">
+        {["1","2","3","4","5"].map((__,index)=>
+        <div key={index}
+        className={`h-[300px] gap-4 flex flex-col min-w-[140px] md:min-w-[250px]  md:h-[520px] md:w-[300px] animate-pulse bg-gray-200 rounded-lg`}
+      >
+        {/* Image Skeleton */}
+        <div className="w-full h-[250px] md:h-[450px] bg-gray-300 rounded-md"></div>
   
-  //       {/* Product Info Skeleton */}
-  //       <div className="md:p-2 p-2">
-  //         <div className="h-5 w-3/4 bg-gray-300 rounded-md"></div>
-  //         <div className="mt-2 h-4 w-1/2 bg-gray-300 rounded-md"></div>
-  //         <div className="mt-1 h-5 w-1/3 bg-gray-300 rounded-md"></div>
-  //       </div>
-  //     </div>
-  //       )}
-  //       </div>
-  //       )}
-  //     </div>
-  //   )
-  // }
+        {/* Product Info Skeleton */}
+        <div className="md:p-2 p-2">
+          <div className="h-5 w-3/4 bg-gray-300 rounded-md"></div>
+          <div className="mt-2 h-4 w-1/2 bg-gray-300 rounded-md"></div>
+          <div className="mt-1 h-5 w-1/3 bg-gray-300 rounded-md"></div>
+        </div>
+      </div>
+        )}
+        </div>
+        )}
+      </div>
+    )
+  }
 
   return (
     <>
@@ -62,7 +71,7 @@ const HomeSub = () => {
       <img src="/story.jpg" className="w-full h-full object-contain" alt="" />
     </div>
     <div className="w-full mt-5">
-      {items?.subCategories?.map((subcategory: SubCategory) => (
+      {subcategories.map((subcategory: SubCategory) => (
         <div key={subcategory._id} className="md:mt-5 w-full px-2 md:px-4">
           {/* Render the special image after the second subcategory */}
           {/* {index === 2 && (
