276f748ca7f4ad267bf1ad925be573e2de38e764
Midhun Kumar <midhun2031@gmail.com>
2025-03-24 16:58:01 +0530

    Add Subcategory Dependency

--- src/components/categoryPage/categoryPage.tsx
+++ src/components/categoryPage/categoryPage.tsx
@@ -1,47 +1,43 @@
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
-import {  Product } from "../../types/CategoryType";
-import ProductCard from "../product/productCard";
-import Loading from "../loading/loading";
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Product } from '../../types/CategoryType';
+import ProductCard from '../product/productCard';
+import Loading from '../loading/loading';
 
 const CategoryPage = () => {
-    const {subcategoryName} = useParams<any>();
-    const [category,setCategory] = useState<any>(null);
-    const [products,setProducts] = useState<Product[]>([]);
-    const [isLoading,setisLoading] = useState(false);
-  
-    async function getcategories(){
-        const response = await fetch(`https://stile-backend.vercel.app/category/${subcategoryName}`);
-        const data:any = await response.json();
-        setCategory(data?.subcategory?.name);
-        setProducts(data?.products)
-    }
-  useEffect(()=>{
-    window.scrollTo(0,0);
+  const { subcategoryName } = useParams<any>();
+  const [category, setCategory] = useState<any>(null);
+  const [products, setProducts] = useState<Product[]>([]);
+  const [isLoading, setisLoading] = useState(false);
+
+  async function getcategories() {
+    const response = await fetch(`https://stile-backend.vercel.app/category/${subcategoryName}`);
+    const data: any = await response.json();
+    setCategory(data?.subcategory?.name);
+    setProducts(data?.products);
+  }
+  useEffect(() => {
+    window.scrollTo(0, 0);
     setisLoading(true);
     getcategories();
     setTimeout(() => {
-      setisLoading(false)
+      setisLoading(false);
     }, 700);
-  },[])
-  if(isLoading){
-    return <Loading />
+  }, [subcategoryName]);
+  if (isLoading) {
+    return <Loading />;
   }
 
   return (
-   
     <div className="mt-12 md:mt-5 md:py-4 pb-2 px-2 md:px-4">
-      <h1 className="text-sm md:text-xl  font-bold  text-gray-800 mb-4">
-        {category}
-      </h1>
+      <h1 className="text-sm md:text-xl  font-bold  text-gray-800 mb-4">{category}</h1>
       <div className="flex justify-between sm:justify-start flex-wrap gap-[2px] md:gap-5 ">
-        {products?.map((product:any) => (
-           <ProductCard product={product} />
-        )
-          )}
+        {products?.map((product: any) => (
+          <ProductCard product={product} />
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CategoryPage
+export default CategoryPage;
